59c016656934be7ed557e8cad33f76452f50d14c
Anna Nowak <ann.kawulaa@gmail.com>
2024-02-10 16:34:09 +0100

    styled app

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import BusinessList from './BusinessList.js'
 function App() {
   return (
     <div className="app">
-      <div>
-        <button>Best Match</button>
-        <button>Most Reviewed</button>
-        <button>Highest Rated</button>
+      <div className='banner'>
+        ravenous
       </div>
-      <div>
-        <input placeholder='Search Business' />
-        <input placeholder='Where?'/>
-      </div>
-      <div>
-        <button>Let's Go</button>
+      <div className='header'>
+        <div >
+          <button className='sorting-button'>Best Match</button>
+          <button className='sorting-button'>Most Reviewed</button>
+          <button className='sorting-button'>Highest Rated</button>
+        </div>
+        <div className='inputs'>
+          <input className='search' placeholder='Search Business' />
+          <input className='search' placeholder='Where?'/>
+        </div>
+        <div>
+          <button className='search-button'>Let's Go</button>
+        </div>
       </div>
 
       <div className='container'>
