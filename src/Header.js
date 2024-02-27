43b7c881fb373daa604ca836dbee8cf6feaf3da6
Anna Nowak <ann.kawulaa@gmail.com>
2024-02-27 16:07:50 +0100

    final changes

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,18 @@
-import { useState } from 'react';
-import './Header.css';
-import { callYelpApi } from './api/client';
+import { useState } from 'react'
+import './Header.css'
+import { callYelpApi } from './api/client'
 
 function Header(props) {
   const [value, setValue] = useState('best_match')
-  const [searchText, setSearchText] = useState("")
-  const [whereText, setWhereText] = useState("")
+  const [searchText, setSearchText] = useState('')
+  const [whereText, setWhereText] = useState('')
 
   function handleWhereChange(event) {
     const where = event.target.value
     setWhereText(where)
   }
 
-  function handleSearchChange(event){
+  function handleSearchChange(event) {
     const newText = event.target.value
     setSearchText(newText)
   }
@@ -23,14 +23,14 @@ function Header(props) {
     searchWith({
       search: searchText,
       location: whereText,
-      sorting: buttonValue
+      sorting: buttonValue,
     })
   }
 
   function searchWith(query) {
     let businessesPromise = callYelpApi(query)
 
-    businessesPromise.then(json => {
+    businessesPromise.then((json) => {
       props.onSearch(json.businesses)
     })
   }
@@ -39,32 +39,62 @@ function Header(props) {
     const q = {
       search: searchText,
       location: whereText,
-      sorting: value
+      sorting: value,
     }
 
     searchWith(q)
   }
 
   function buttonClass(buttonId) {
-    return `${value === buttonId ? 'clicked' : '' } sorting-button`
+    return `${value === buttonId ? 'clicked' : ''} sorting-button`
   }
 
   return (
-    <div className='header'>
+    <div className="header">
       <div>
-        <button value='best_match' onClick={handleClick} className={buttonClass('best_match')}>Best Match</button>
-        <button value='review_count' onClick={handleClick} className={buttonClass('review_count')}>Most Reviewed</button>
-        <button value='rating' onClick={handleClick} className={buttonClass('rating')}>Highest Rated</button>
+        <button
+          value="best_match"
+          onClick={handleClick}
+          className={buttonClass('best_match')}
+        >
+          Best Match
+        </button>
+        <button
+          value="review_count"
+          onClick={handleClick}
+          className={buttonClass('review_count')}
+        >
+          Most Reviewed
+        </button>
+        <button
+          value="rating"
+          onClick={handleClick}
+          className={buttonClass('rating')}
+        >
+          Highest Rated
+        </button>
       </div>
-      <div className='inputs'>
-        <input value={searchText} onChange={handleSearchChange} className='search' placeholder='Search Business' />
-        <input value={whereText} onChange={handleWhereChange} className='search' placeholder='Where?'/>
+      <div className="inputs">
+        <input
+          value={searchText}
+          onChange={handleSearchChange}
+          className="search"
+          placeholder="Search Business"
+        />
+        <input
+          value={whereText}
+          onChange={handleWhereChange}
+          className="search"
+          placeholder="Where?"
+        />
       </div>
       <div>
-        <button onClick={handleDoSearch} className='search-button'>Let's Go</button>
+        <button onClick={handleDoSearch} className="search-button">
+          Let's Go
+        </button>
       </div>
     </div>
   )
 }
 
-export default Header;
+export default Header
--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,9 @@
-const key= process.env["REACT_APP_YELP_API_KEY"]
+const key = process.env['REACT_APP_YELP_API_KEY']
 
-if(key === undefined) {
-  throw new Error("REACT_APP_YELP_API_KEY variable is not set. Please provide ApiKey")
+if (key === undefined) {
+  throw new Error(
+    'REACT_APP_YELP_API_KEY variable is not set. Please provide ApiKey'
+  )
 }
 
 const PROXY_SERVER_URL = 'https://cors-anywhere.herokuapp.com'
@@ -10,13 +12,19 @@ const YELP_API_URL = 'https://api.yelp.com/v3'
 const API_URL = `${PROXY_SERVER_URL}/${YELP_API_URL}`
 
 export function callYelpApi(query) {
-  const options = {method: 'GET', headers: {accept: 'application/json', authorization: `Bearer ${key}`}};
+  const options = {
+    method: 'GET',
+    headers: { accept: 'application/json', authorization: `Bearer ${key}` },
+  }
 
-  const promise = fetch(`${API_URL}/businesses/search?sort_by=${query.sorting}&limit=20&location=${query.location.length > 0 ? query.location : 'NYC'}&term=${query.search}`, options)
+  const promise = fetch(
+    `${API_URL}/businesses/search?sort_by=${query.sorting}&limit=20&location=${query.location.length > 0 ? query.location : 'NYC'}&term=${query.search}`,
+    options
+  )
 
   return promise
-    .then(response => response.json())
-    .catch(err => {
+    .then((response) => response.json())
+    .catch((err) => {
       return console.error(err)
-    });
+    })
 }
